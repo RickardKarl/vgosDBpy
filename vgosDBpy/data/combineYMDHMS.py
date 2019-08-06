b0562e3d304f748ab74a9f0801fb9e3dc52a6d68
Rickard Karlsson <35599490+RickardKarl@users.noreply.github.com>
2019-08-06 15:46:23 -0400

    Work in progress

--- vgosDBpy/data/combineYMDHMS.py
+++ vgosDBpy/data/combineYMDHMS.py
@@ -1,22 +1,23 @@
 from netCDF4 import Dataset
 import pandas as pd
 import os
-
 """
 Takes in a path to a TimeUTC.nc file and combines the YMDHM data
 with the second to create a YMDHMS format.
 Uses panda to create Timestamp.
 """
 def combineYMDHMwithSec(timeFilePath):
-    with Dataset(timeFilePath,"r", format="NETCDF4_CLASSIC") as time :
+    '''
+    Note: Executes very slowly because of the loop, no fix found.
+    '''
+    with Dataset(timeFilePath,"r", format="NETCDF4_CLASSIC") as time_file:
 
-        seconds = time.variables["Second"]
-        YMDHM = time.variables["YMDHM"]
+        seconds = time_file.variables["Second"]
+        YMDHM = time_file.variables["YMDHM"]
         if len(seconds) != len(YMDHM):
             return null
 
         YMDHMS = []
-
         for i in range(len(YMDHM)):
             YMDHMS.append(pd.Timestamp(int(YMDHM[i][0]),int(YMDHM[i][1]),int(YMDHM[i][2]),int(YMDHM[i][3]),int(YMDHM[i][4]),int(seconds[i])))
     return YMDHMS
